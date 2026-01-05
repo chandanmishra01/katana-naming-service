e5755d3ed1b994076a9026b0c4fbeed81b3c771a
Chandan Kumar <chandankumar@Chandans-MacBook-Pro.local>
2026-01-06 01:06:40 +0530

    chore: update meta intro

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // next.config.js
+  eslint: { ignoreDuringBuilds: true },
   reactStrictMode: true,
   images: {
     domains: ["raw.githubusercontent.com"],
